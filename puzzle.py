9f4455696ffa4e1eb1e2a94846656f46e9954ba6
Sai Lokesh Reddy Pannala <sailokesh365@gmail.com>
2026-04-28 16:17:46 +0530

    Create puzzle.py

---
+++ puzzle.py
@@ -0,0 +1,16 @@
+legs = int(input("Enter total legs: "))
+heads = int(input("Enter total heads: "))
+
+flag = False
+for cows in range(0, heads + 1):
+    hens = heads - cows
+    cal_legs = cows * 4 + hens * 2
+    
+    if cal_legs == legs:
+        flag = True
+        break  
+if flag:
+    print("Cows:", cows)
+    print("Hens:", hens)
+else:
+    print("Not possible")
